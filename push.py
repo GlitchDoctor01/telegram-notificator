e32769ef1fbce6b37eb0b219170bbd2162eb9ea4
Glitch <69388702+GlitchDoctor01@users.noreply.github.com>
2021-01-02 18:59:04 +0600

    Update push.py

--- push.py
+++ push.py
@@ -1,5 +1,4 @@
 
-import os
 import argparse
 import urllib.request
 
@@ -9,13 +8,10 @@ ap.add_argument("-c", "--chat", required=True, help="Chat id to notify")
 ap.add_argument("-m", "--message", required=True, help="Text of the message")
 args = vars(ap.parse_args())
 
-
-encoded_msg = args["message"].replace(" ", "%20")
-
 url = "https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}".format(
 	token = args["token"],
 	chat_id = args["chat"],
-	message = encoded_msg)
+	message = args["message"].replace(" ", "%20"))
 
 print(url)
 urllib.request.urlopen(url).read()
